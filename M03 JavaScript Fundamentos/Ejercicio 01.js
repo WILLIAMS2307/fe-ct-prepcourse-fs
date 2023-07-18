67610e0b50fb81150cc61d1ec6a798a7de46a3fa
William Sanchez <118145847+WILLIAMS2307@users.noreply.github.com>
2023-07-17 20:45:35 -0500

    Update Ejercicio 01.js
    Desarrollo Ejercicio 01

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -6,21 +6,29 @@
 
 // Crea una variable de tipo string.
 const nuevoString = null;
+var nuevoString = "PEDRO";
 
 // Crea una variable de tipo number.
 const nuevoNumero = null;
+var  nuevoNumero = "5";
 
 // Crea una variable de tipo boolean.
 const nuevoBoolean = null;
+var estaLloviendo = false;
+var estaHaciendoSol = true;
 
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - null === 3;
+console.log (10-7);
+
 
 // Resuelve el siguiente problema matemático.
 const nuevaMultiplicacion = 10 * null === 40;
+console.log (10*4);
 
 // Resuelve el siguiente problema matemático.
 const nuevoModulo = 21 % 5 === null;
+console.log (21/5);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
